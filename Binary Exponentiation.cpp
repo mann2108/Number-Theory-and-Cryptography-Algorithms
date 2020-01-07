87d8fe635063e404fc0dd99bb5d9576f14f84285
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-01-08 00:47:18 +0530

    Update Binary Exponentiation.cpp

--- Binary Exponentiation.cpp
+++ Binary Exponentiation.cpp
@@ -1,5 +1,18 @@
+/**
+Binary exponentiation (also known as exponentiation by squaring)
+is a trick which allows to calculate an using only O(logn) multiplications
+(instead of O(n) multiplications required by the naive approach).
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
 int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        /// Actual Implementation
 
+
+    }
 }
